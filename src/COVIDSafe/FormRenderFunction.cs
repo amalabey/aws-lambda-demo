9cf46429ac45917400e96e6e1986f4250739dc4a
Amal Abeygunawardana <amalabey@gmail.com>
2021-03-09 13:05:30 +1100

    Complete registration form rendering

--- src/COVIDSafe/FormRenderFunction.cs
+++ src/COVIDSafe/FormRenderFunction.cs
@@ -5,6 +5,8 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using HandlebarsDotNet;
+using System;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -18,28 +20,99 @@ namespace COVIDSafe
 
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
         {
-            var locationId = apigProxyEvent.QueryStringParameters["id"];
-            var locationItem = await GetLocationNameAsync(locationId);
-            var locationName = locationItem["name"];
-
-            var body = $"<html><head>Test Form</head><body><h3>Register for Location {locationName} <br/>querystring: {string.Join(",", apigProxyEvent.QueryStringParameters.Keys)}  <br/>body: {apigProxyEvent.Body}</body></html>";
-            return new APIGatewayProxyResponse
+            try 
             {
-                Body = body,
-                StatusCode = 200,
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/html" } }
-            };
+                var locationId = apigProxyEvent.QueryStringParameters["id"];
+                var locationItem = await GetLocationNameAsync(locationId);
+                var locationName = locationItem["name"];
+                var submitUrl = $"https://{apigProxyEvent.RequestContext.DomainName}/Prod/register";
+                LambdaLogger.Log($"locationName: {locationName}, submitUrl: {submitUrl}");
+
+                var html = RenderRegistrationForm(locationId, locationName, submitUrl);
+                LambdaLogger.Log($"rendered html: {html}");
+
+                return new APIGatewayProxyResponse
+                {
+                    Body = html,
+                    StatusCode = 200,
+                    Headers = new Dictionary<string, string> { { "Content-Type", "text/html" } }
+                };
+            }catch(Exception ex)
+            {
+                LambdaLogger.Log($"exception message: {ex.Message}");
+                LambdaLogger.Log($"stack trace: {ex.StackTrace}");
+                throw;
+            }
         }
 
         private async Task<Document> GetLocationNameAsync(string locationId)
         {
-            LambdaLogger.Log($"Location id: {locationId}");
             var locationsTable = Table.LoadTable(_dynamoDbClient, LOCATIONS_TABLE_NAME);
-            LambdaLogger.Log($"LocationsTable null?: {locationsTable == null}");
             var hash = new Primitive(locationId, false);
             var locationItem = await locationsTable.GetItemAsync(hash);
-            LambdaLogger.Log($"Retrieved item from dynamodb: {locationItem}");
             return locationItem;
         }
+
+        private string RenderRegistrationForm(string locationId, string locationName, string submitUrl)
+        {
+            string formTemplate = @"
+            <html>
+            <head>
+                <meta content='text/html;charset=utf-8' http-equiv='Content-Type'>
+                <meta content='utf-8' http-equiv='encoding'>
+                <title>Register</title>
+                <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+                <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl' crossorigin='anonymous'>
+            </head>
+            <body>
+                <form>
+                    <h3> You are visiting {{locationName}} </h3>
+                    <p> Please register using below form. </p>
+                    <input type='hidden' id='locationId' value={{locationId}}/><br/>
+                    <div class='form-group'>
+                        <label for='fullName'>Name:</label>
+                        <input type='text' id='fullName' placeholder='Enter full name' class='form-control'><br/>
+                    </div>
+                    <div class='form-group'>
+                        <label for='phone'>Phone:</label>
+                        <input type='text' id='phone' placeholder='Phone number' class='form-control'><br/>
+                    </div>
+                    <button id='submit' type='submit' class='btn btn-primary'>Submit</button>
+                </form>
+                <script>
+                    const submit = document.querySelector('#submit')
+                    const fullName = document.querySelector('#fullName')
+                    const phone = document.querySelector('#phone')
+
+                    submit.onclick = (e) => {
+                        e.preventDefault();
+                        axios.post('{{submitUrl}}', {
+                            name: fullName.value,
+                            phone: phone.value
+                        },
+                        {
+                            headers: {
+                                ContentType: 'application/json'
+                            }
+                        })
+                        .then(res => {
+                        })
+                        .catch(err => {
+                        })
+                    }
+                </script>
+            </body>
+            </html>";
+
+            var template = Handlebars.Compile(formTemplate);
+            var data = new {
+                locationId = locationId,
+                locationName = locationName,
+                submitUrl = submitUrl
+            };
+            var result = template(data);
+            return result;
+        }
+
     }
 }
